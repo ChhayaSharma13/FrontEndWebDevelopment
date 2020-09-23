ab9d3b5792856f5b99331797f88ed6548b5261da
Chhaya Sharma <Chhayabhardwaj1998@gmail.com>
2020-09-23 19:59:00 +0100

    createServer method

--- NodeJS/Node1.js
+++ NodeJS/Node1.js
@@ -1,9 +1,17 @@
 // console.log("Hello World");
-
+// require() is used to load and cache JavaScript  modules
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+// hhtp.createServer() method turns our computer into an HTTP Server.
+// https.createServer()  creates an HTTP Server Object
+// http Server Object can listen to ports on our cmputer and execute a function, a requestListener, each time a request is made
+
+
+// Syntax - http.createServer(requestListener);
+// requestListener - optional. Specifies a function to be executed 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
